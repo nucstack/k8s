ec587e786e22fbeb27853bdb2ce6c3550c804c20
dmcavinue <dan@mcavinue.io>
2021-04-26 17:03:39 -0400

    drop duplication

--- scripts/initialize.sh
+++ scripts/initialize.sh
@@ -3,6 +3,3 @@
 set -eou pipefail
 
 kubectl --kubeconfig=./kubeconfig apply --kustomize=./cluster/base/flux-system
-
-# TODO: remove this double run requirement
-kubectl --kubeconfig=./kubeconfig apply --kustomize=./cluster/base/flux-system
