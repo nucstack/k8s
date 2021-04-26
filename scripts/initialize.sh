1d5945695b4b1d4e1513f79c1787f6334aceeede
dmcavinue <dan@mcavinue.io>
2021-04-26 17:01:24 -0400

    lazy double run fix (meh)

--- scripts/initialize.sh
+++ scripts/initialize.sh
@@ -3,3 +3,6 @@
 set -eou pipefail
 
 kubectl --kubeconfig=./kubeconfig apply --kustomize=./cluster/base/flux-system
+
+# TODO: remove this double run requirement
+kubectl --kubeconfig=./kubeconfig apply --kustomize=./cluster/base/flux-system
