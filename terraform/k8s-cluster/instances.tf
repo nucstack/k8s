217e8e0f3a4db59a4206135f408b0c6cc9ef60d2
dmcavinue <dan@mcavinue.io>
2021-04-27 10:28:11 -0400

    relocate state to be k8s provider agnostic

---
+++ terraform/k8s-cluster/instances.tf
@@ -0,0 +1,36 @@
+// define our tfvars
+variable name              {}
+variable description       {}
+variable username          {}
+variable password          {}
+variable environment       {}
+variable instances         {}
+variable template          {}
+variable cpu_sockets       {}
+variable cpu_cores         {}
+variable memory            {}
+variable disk              {}
+variable network           {}
+variable nameserver        {}
+
+module "k3s" {
+  source      = "../modules/proxmox-vm"
+
+  name              = var.name
+  description       = var.description
+  instances         = var.instances
+  environment       = var.environment
+  template          = var.template
+  username          = var.username
+  password          = var.password
+
+  // resources    
+  cpu_sockets       = var.cpu_sockets 
+  cpu_cores         = var.cpu_cores 
+  memory            = var.memory
+  disk              = var.disk
+
+  // network  
+  network           = var.network
+  nameserver        = var.nameserver
+}
