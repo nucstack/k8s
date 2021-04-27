621f8f87e5d72a21fcf63328521f538f5985d645
dmcavinue <dan@mcavinue.io>
2021-04-27 10:41:20 -0400

    export pub key

--- terraform/modules/proxmox-vm/file.tf
+++ terraform/modules/proxmox-vm/file.tf
@@ -3,3 +3,8 @@ resource "local_file" "ssh_private_key" {
   content  = tls_private_key.ssh_key.private_key_pem
   filename = "${var.environment}_ssh_private_key"
 }
+
+resource "local_file" "ssh_public_key" {
+  content  = tls_private_key.ssh_key.public_key_openssh
+  filename = "${var.environment}_ssh_public_key"
+}
