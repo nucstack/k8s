f3aa4fe768ff19eaf440912471754040a29e2848
Daniel McAvinue <dan@mcavinue.io>
2021-04-28 09:47:07 -0400

    add botkube (#13)
    * add botkube
    
    * add disable linter

--- scripts/secrets.sh
+++ scripts/secrets.sh
@@ -10,6 +10,10 @@ sops --encrypt --in-place ./cluster/core/security/authelia/authelia-postgres.sec
 envsubst < ./cluster/core/security/authelia/authelia-redis.secret.enc.template > ./cluster/core/security/authelia/authelia-redis.secret.enc.yaml
 sops --encrypt --in-place ./cluster/core/security/authelia/authelia-redis.secret.enc.yaml
 
+# botkube
+envsubst < ./cluster/apps/observability/botkube/botkube.secrets.enc.template > ./cluster/apps/observability/botkube/botkube.secrets.enc.yaml
+sops --encrypt --in-place ./cluster/apps/observability/botkube/botkube.secrets.enc.yaml
+
 # openldap
 envsubst < ./cluster/core/security/openldap/openldap.secret.enc.template > ./cluster/core/security/openldap/openldap.secret.enc.yaml
 sops --encrypt --in-place ./cluster/core/security/openldap/openldap.secret.enc.yaml
