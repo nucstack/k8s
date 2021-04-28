410fcb08d7d2d430ccfe760ad1a380e77aeb009f
Daniel McAvinue <dan@mcavinue.io>
2021-04-28 10:23:08 -0400

    add prometheus stack (#14)
    * add prometheus stack
    
    * bugfix

--- scripts/secrets.sh
+++ scripts/secrets.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+envsubst < ./tmpl/cluster-secrets.yaml > ./cluster/base/cluster-secrets.yaml
+envsubst < ./tmpl/cluster-settings.yaml > ./cluster/base/cluster-settings.yaml
+sops --encrypt --in-place ./cluster/base/cluster-secrets.yaml
 
 # authelia
 envsubst < ./cluster/core/security/authelia/authelia.secret.enc.template > ./cluster/core/security/authelia/authelia.secret.enc.yaml
@@ -13,6 +16,13 @@ sops --encrypt --in-place ./cluster/core/security/authelia/authelia-redis.secret
 # botkube
 envsubst < ./cluster/apps/observability/botkube/botkube.secrets.enc.template > ./cluster/apps/observability/botkube/botkube.secrets.enc.yaml
 sops --encrypt --in-place ./cluster/apps/observability/botkube/botkube.secrets.enc.yaml
+
+# kube-prometheus-stack
+envsubst < ./cluster/apps/observability/kube-prometheus-stack/grafana-ldap-config.secrets.enc.template > ./cluster/apps/observability/kube-prometheus-stack/grafana-ldap-config.secrets.enc.yaml
+sops --encrypt --in-place ./cluster/apps/observability/kube-prometheus-stack/grafana-ldap-config.secrets.enc.yaml
+
+envsubst < ./cluster/apps/observability/kube-prometheus-stack/grafana-datasources.secrets.enc.template > ./cluster/apps/observability/kube-prometheus-stack/grafana-datasources.secrets.enc.yaml
+sops --encrypt --in-place ./cluster/apps/observability/kube-prometheus-stack/grafana-datasources.secrets.enc.yaml
 
 # openldap
 envsubst < ./cluster/core/security/openldap/openldap.secret.enc.template > ./cluster/core/security/openldap/openldap.secret.enc.yaml
