36d1ed93a11c8bab7b1e7db06bb6a9c95300623f
dmcavinue <dan@mcavinue.io>
2021-04-28 11:46:39 -0400

    Add prometheus deployment

--- scripts/secrets.sh
+++ scripts/secrets.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-envsubst < ./tmpl/cluster-secrets.yaml > ./cluster/base/cluster-secrets.yaml
 envsubst < ./tmpl/cluster-settings.yaml > ./cluster/base/cluster-settings.yaml
+envsubst < ./tmpl/cluster-secrets.yaml > ./cluster/base/cluster-secrets.yaml
 sops --encrypt --in-place ./cluster/base/cluster-secrets.yaml
 
 # authelia
@@ -16,6 +16,10 @@ sops --encrypt --in-place ./cluster/core/security/authelia/authelia-redis.secret
 # botkube
 envsubst < ./cluster/apps/observability/botkube/botkube.secrets.enc.template > ./cluster/apps/observability/botkube/botkube.secrets.enc.yaml
 sops --encrypt --in-place ./cluster/apps/observability/botkube/botkube.secrets.enc.yaml
+
+# Kube-prometheus-stack
+envsubst < ./cluster/apps/observability/kube-prometheus-stack/grafana-ldap.secret.enc.template > ./cluster/apps/observability/kube-prometheus-stack/grafana-ldap.secret.enc.yaml
+sops --encrypt --in-place ./cluster/apps/observability/kube-prometheus-stack/grafana-ldap.secret.enc.yaml
 
 # influxdb
 envsubst < ./cluster/apps/observability/influxdb/influxdb.secret.enc.template > ./cluster/apps/observability/influxdb/influxdb.secret.enc.yaml
