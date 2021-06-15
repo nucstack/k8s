d98b3c82c1acb1faced24d0f54e4db5934c67d61
dmcavinue <dan@mcavinue.io>
2021-06-14 20:29:21 -0400

    drop var

--- terraform/modules/instance/aws.tf
+++ terraform/modules/instance/aws.tf
@@ -25,7 +25,6 @@ data "template_file" "bootstrap" {
   template = file("${path.module}/scripts/k3s-bootstrap.sh.tmpl")
   vars = {
     TAILSCALE_AUTH_KEY = var.tailscale_auth_key
-    K8S_TOKEN          = var.k8s_token
   }
 }
 
