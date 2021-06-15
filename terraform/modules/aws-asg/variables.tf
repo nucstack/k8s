306a6cc5e7e80ad5128a97038a68530ea8e625f1
dmcavinue <dan@mcavinue.io>
2021-06-15 16:58:40 -0400

    somewhat modularized aws resources

---
+++ terraform/modules/aws-asg/variables.tf
@@ -0,0 +1,62 @@
+
+variable "application_name" {
+  description = "application_name"
+  type        = string
+}
+
+variable "environment" {
+  description = "The Deployment environment"
+}
+
+variable "subnet_id" {
+  description = "subnet id"
+}
+
+variable "image_name" {
+  type        = string
+  description = "subnet id"
+}
+
+variable "availability_zones" {
+  type        = list
+  description = "The az that the resources will be launched"
+}
+
+variable "instance_type" {
+  type        = string
+  description = "subnet id"
+  default     = "t2.micro"
+}
+
+variable "security_group_id" {
+  type        = string
+  description = "security group id"
+}
+
+variable "tags" {
+  type        = map(string)
+  description = "tags"
+}
+
+variable "bootstrap_vars" {
+  type        = map(string)
+  description = "bootstrap_vars"
+}
+
+variable "desired_capacity" {
+  type        = number
+  description = "desired_capacity"
+  default     = 1
+}
+
+variable "max_size" {
+  type        = number
+  description = "max_size"
+  default     = 1
+}
+
+variable "min_size" {
+  type        = number
+  description = "min_size"
+  default     = 1
+}
