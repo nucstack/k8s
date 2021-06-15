6dd352975a893020bdb91fbefa69139cfd4312dc
dmcavinue <dan@mcavinue.io>
2021-06-15 16:58:40 -0400

    somewhat modularized aws resources

---
+++ terraform/modules/aws-asg/main.tf
@@ -0,0 +1,74 @@
+
+data "aws_ami" "image" {
+  most_recent = true
+  filter {
+    name   = "name"
+    values = [var.image_name]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+  owners = ["self"]
+}
+
+resource "tls_private_key" "ssh_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+// ssh key pair
+resource "aws_key_pair" "ssh-key-pair" {
+  key_name   = "${var.application_name}-${var.environment}"
+  public_key = tls_private_key.ssh_key.public_key_openssh
+}
+
+// bootstrap script
+data "template_file" "bootstrap-script" {  
+  template = file("${path.module}/scripts/bootstrap-${var.application_name}.sh.tmpl")
+  vars = var.bootstrap_vars
+}
+
+// instance launch template
+resource "aws_launch_template" "instance-template" {
+  name_prefix   = var.application_name
+  image_id      = data.aws_ami.image.id
+  instance_type = var.instance_type
+  metadata_options {
+    http_endpoint = "enabled"
+    http_tokens = "required"
+    http_put_response_hop_limit = 1
+  }
+  network_interfaces {
+    associate_public_ip_address = false
+    subnet_id = var.subnet_id
+    security_groups = [
+      var.security_group_id
+    ]
+  }
+  key_name = aws_key_pair.ssh-key-pair.key_name
+  user_data = base64encode(data.template_file.bootstrap-script.rendered)
+
+  tag_specifications {
+    resource_type = "instance"
+    tags          = var.tags
+  }
+}
+
+// auto scaling group for k3s instances
+resource "aws_autoscaling_group" "auto-scaling-group" {
+  name               = var.application_name
+  availability_zones = var.availability_zones
+  desired_capacity   = var.desired_capacity
+  max_size           = var.max_size
+  min_size           = var.min_size
+
+  launch_template {
+    id      = aws_launch_template.instance-template.id
+    version = "$Latest"
+  }
+}
--- terraform/modules/instance/aws.tf
+++ terraform/modules/instance/aws.tf
@@ -1,98 +1,64 @@
 
-resource "aws_key_pair" "ssh-key" {
-  count      = var.type == "aws" ? 1 : 0
-  key_name   = var.name
-  public_key = var.ssh_authorized_key != "" ? var.ssh_authorized_key : tls_private_key.ssh_key.public_key_openssh
+variable "region" {
+  description = "region"
+  type        = string
+  default     = "us-east-2"
 }
 
-resource "aws_security_group" "ssh" {
-  count  = var.type == "aws" ? 1 : 0
-  name   = "allow-ssh"
-  description = "Allow TLS inbound traffic"  
-  ingress {
-    description      = "SSH from anywhere"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]    
-  }
-  egress {
-    description      = "All egress access"
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]    
-  }
-
-  tags = {
-    Name = "allow_ssh"
-  }
+variable "availability_zones" {
+  description = "availability zones"
+  type        = list
+  default     = ["us-east-2a"] 
 }
 
-data "aws_ami" "k3s-image" {
-  count       = var.type == "aws" ? 1 : 0
-  most_recent = true
-  filter {
-    name   = "name"
-    values = ["k3s-*"]
-  }
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-  filter {
-    name   = "root-device-type"
-    values = ["ebs"]
-  }
-  owners = ["self"]
+variable "vpc_cidr" {
+  description = "vpc_cidr zones"
+  type        = string
+  default     = "10.0.0.0/16"
 }
 
-data "template_file" "bootstrap" {
-  count    = var.type == "aws" ? 1 : 0
-  template = file("${path.module}/scripts/k3s-bootstrap.sh.tmpl")
-  vars = {
+variable "public_subnets_cidr" {
+  description = "public subnets"
+  type        = list
+  default     = ["10.0.1.0/24"] 
+}
+
+variable "private_subnets_cidr" {
+  description = "private subnets"
+  type        = list
+  default     = ["10.0.10.0/24"]
+}
+
+// vpc, subnets, gws, routes
+module "networking" {
+  source = "../../modules/aws-networking"
+  count                = var.type == "aws" ? 1 : 0
+  region               = var.region
+  environment          = var.environment
+  vpc_cidr             = var.vpc_cidr
+  public_subnets_cidr  = var.public_subnets_cidr
+  private_subnets_cidr = var.private_subnets_cidr
+  availability_zones   = var.availability_zones
+}
+
+// auto scaling instances
+module "asg-instances" {
+  source             = "../../modules/aws-asg"
+  count              = var.type == "aws" ? length(tolist(element(module.networking.0.private_subnets_id, 0))) : 0
+  application_name   = "k3s"
+  environment        = var.environment
+  subnet_id          = element(tolist(element(module.networking.0.private_subnets_id, 0)), count.index)
+  image_name         = "k3s-*"
+  availability_zones = var.availability_zones  
+  security_group_id  = module.networking.0.default_sg_id
+  tags               = var.tags
+  desired_capacity   = 1
+  max_size           = 1
+  min_size           = 1
+  bootstrap_vars     = {
     TAILSCALE_AUTH_KEY = var.tailscale_auth_key
   }
-}
-
-resource "aws_launch_template" "k3s" {
-  count         = var.type == "aws" ? 1 : 0
-  name_prefix   = var.name
-  image_id      = data.aws_ami.k3s-image.0.id
-  instance_type = "t2.micro"
-  instance_market_options {
-    market_type = "spot"
-  }
-  metadata_options {
-    http_endpoint = "enabled"
-    http_tokens = "required"
-    http_put_response_hop_limit = 1
-  }
-  network_interfaces {
-    associate_public_ip_address = false
-    security_groups = [
-      aws_security_group.ssh.0.id
-    ]
-  }
-  key_name = aws_key_pair.ssh-key.0.key_name
-  user_data = base64encode(data.template_file.bootstrap.0.rendered)
-
-  tag_specifications {
-    resource_type = "instance"
-    tags = var.tags
-  }
-}
-
-resource "aws_autoscaling_group" "k3s" {
-  count              = var.type == "aws" ? 1 : 0
-  name               = var.name
-  availability_zones = ["us-east-2a"]
-  desired_capacity   = length(var.instances)
-  max_size           = length(var.instances)
-  min_size           = 1
-
-  launch_template {
-    id      = aws_launch_template.k3s.0.id
-    version = "$Latest"
-  }
+  depends_on = [
+    module.networking
+  ]
 }
