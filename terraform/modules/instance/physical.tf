af41c519d56e1ba1d8743729565a124278944b6d
Daniel McAvinue <dan@mcavinue.io>
2021-08-07 21:18:39 -0400

    changes to resolve production node and env (#345)

---
+++ terraform/modules/instance/physical.tf
@@ -0,0 +1,23 @@
+
+resource "null_resource" "node_info" {
+  count = length(var.instances)
+
+  provisioner "file" {
+    destination = "/home/${var.username != "" ? var.username : random_pet.username.id}/.ssh/authorized_keys"
+    content     = tls_private_key.ssh_key.public_key_openssh
+
+    connection {
+      type        = "ssh"
+      user        = var.username != "" ? var.username : random_pet.username.id
+      password    = var.password != "" ? var.password : random_password.password.result
+      host        = element(var.instances, count.index)
+    }
+  }
+
+  triggers = {
+    name               = format("%s-%s", var.name, element(random_string.suffix, count.index).result)
+    username           = var.username != "" ? var.username : random_pet.username.id
+    ipaddress          = element(var.instances, count.index)
+    ssh_authorized_key = var.ssh_authorized_key != "" ? var.ssh_authorized_key : tls_private_key.ssh_key.public_key_openssh
+  }
+}
